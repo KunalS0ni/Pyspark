763ddd7053fd8eceb2b0fe64bdd892dfb64c3bbc
kunal <mail.kunalsoni@gmail.com>
2024-06-03 11:22:19 +0530

    Added Import Spark and viewing version

--- import_pyspark.py
+++ import_pyspark.py
@@ -1,2 +1,8 @@
 import findspark
 findspark.init()
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Import Pyspark and spark version').getOrCreate()
+
+print(spark.version)
